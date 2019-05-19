b3bf77a1c6658de9c70844e09c3004e04022b5c2
Serge Maslyakov <rusoil.9@gmail.com>
2019-05-20 00:43:58 +0400

    - wip

--- Core/Utils/QRCodeUtils.swift
+++ Core/Utils/QRCodeUtils.swift
@@ -3,7 +3,7 @@ import RxSwift
 
 public enum QRCodeUtils {
 
-    static func generateQRCode(for message: String) -> Observable<UIImage>{
+    static func generateQRCode(for message: String) -> Observable<UIImage> {
 
         return Observable.create { observer -> Disposable in
 
