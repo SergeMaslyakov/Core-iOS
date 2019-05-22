df393c0f3881fac6c9bba44ba3eb5de30ab49a65
Serge Maslyakov <rusoil.9@gmail.com>
2019-05-22 18:25:50 +0400

    - wip

--- Core/NetworkLayer/Coding/MultipartFormEncoder.swift
+++ Core/NetworkLayer/Coding/MultipartFormEncoder.swift
@@ -5,10 +5,11 @@ public final class MultipartFormEncoder: NetworkRequestEncoding {
 
     private enum EncodingCharacters {
         static let crlf = "\r\n"
+        static let crlfcrlf = "\r\n\r\n"
     }
 
     private enum BoundaryType {
-        case initial, encapsulated, final
+        case initial, final
     }
 
     private let dataKey: String
@@ -31,36 +32,31 @@ public final class MultipartFormEncoder: NetworkRequestEncoding {
     private func createMultipartBody(fromMultipartData data: MultipartData, params: [String: Any]) -> Data {
         var multipartData = Data()
 
-        /// initial
-        multipartData.append(boundaryData(forBoundaryType: .initial, boundary: boundaryToken))
-
-        /// binary data
-        let contentDisposition = "Content-Disposition: form-data; name=\"file\"; filename=\"\(data.filename)\"\r\n"
-
-        multipartData.append(contentDisposition.data(using: .utf8) ?? Data())
-        multipartData.append("Content-Type: \(data.mimeType)\r\n".data(using: .utf8) ?? Data())
-        multipartData.append("Content-Length: \(data.data.count)\r\n\r\n".data(using: .utf8) ?? Data())
-        multipartData.append(data.data)
-        multipartData.append("\r\n".data(using: .utf8) ?? Data())
-
         /// params
         params.forEach { (key: String, value: Any) in
 
             let data = "\(value)".data(using: .utf8) ?? Data()
 
-            multipartData.append(boundaryData(forBoundaryType: .encapsulated, boundary: boundaryToken))
-            multipartData.append("Content-Disposition: form-data; name=\"\(key)\"\r\n".data(using: .utf8) ?? Data())
-            multipartData.append("Content-Transfer-Encoding: binary\r\n".data(using: .utf8) ?? Data())
-            multipartData.append("Content-Type: multipart/form-data; charset=utf-8".data(using: .utf8) ?? Data())
-            multipartData.append("Content-Length: \(data.count)\r\n\r\n".data(using: .utf8) ?? Data())
+            multipartData.append(boundaryData(forBoundaryType: .initial, boundary: boundaryToken))
+
+            multipartData.append("Content-Disposition: form-data; name=\"\(key)\"\(EncodingCharacters.crlf)".data(using: .utf8) ?? Data())
+            multipartData.append("Content-Length: \(data.count)\(EncodingCharacters.crlfcrlf)".data(using: .utf8) ?? Data())
             multipartData.append(data)
-            multipartData.append("\r\n".data(using: .utf8) ?? Data())
+            multipartData.append("\(EncodingCharacters.crlf)".data(using: .utf8) ?? Data())
         }
+
+        /// binary data
+        multipartData.append(boundaryData(forBoundaryType: .initial, boundary: boundaryToken))
+        let contentDisposition = "Content-Disposition: form-data; name=\"file\"; filename=\"\(data.filename)\"\(EncodingCharacters.crlf)"
+
+        multipartData.append(contentDisposition.data(using: .utf8) ?? Data())
+        multipartData.append("Content-Length: \(data.data.count)\(EncodingCharacters.crlf)".data(using: .utf8) ?? Data())
+        multipartData.append("Content-Type: \(data.mimeType)\(EncodingCharacters.crlfcrlf)".data(using: .utf8) ?? Data())
+        multipartData.append(data.data)
+        multipartData.append("\(EncodingCharacters.crlf)".data(using: .utf8) ?? Data())
 
         /// final
         multipartData.append(boundaryData(forBoundaryType: .final, boundary: boundaryToken))
-
-        print(String(data: multipartData, encoding: .utf8))
 
         return multipartData
     }
@@ -71,10 +67,8 @@ public final class MultipartFormEncoder: NetworkRequestEncoding {
         switch type {
         case .initial:
             boundaryText = "--\(boundary)\(EncodingCharacters.crlf)"
-        case .encapsulated:
-            boundaryText = "\(EncodingCharacters.crlf)--\(boundary)\(EncodingCharacters.crlf)"
         case .final:
-            boundaryText = "\(EncodingCharacters.crlf)--\(boundary)--\(EncodingCharacters.crlf)"
+            boundaryText = "--\(boundary)--\(EncodingCharacters.crlf)"
         }
 
         return boundaryText.data(using: .utf8, allowLossyConversion: false) ?? Data()
