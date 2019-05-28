a5af31c941fa90d7c820e621fbd1c4b598ccecd7
Serge Maslyakov <rusoil.9@gmail.com>
2019-05-29 01:30:11 +0400

    - wip

--- Core/Utils/TimeFormattingUtils.swift
+++ Core/Utils/TimeFormattingUtils.swift
@@ -3,7 +3,7 @@ import Foundation
 public enum TimeFormatter {
 
     /// string like 01:59:01
-    static func timeFrom(seconds: Int) -> String {
+    public static func timeFrom(seconds: Int) -> String {
 
         func stringify(value: Int) -> String {
             return value < 10 ? "0\(value)" : "\(value)"
@@ -18,5 +18,27 @@ public enum TimeFormatter {
         } else {
             return "\(mins):\(sesc)"
         }
+    }
+
+    /// hours from string looks like "HH:mm:ss"
+    public static func hours(from str: String) -> Int? {
+        let comp = str.components(separatedBy: ":")
+
+        if let h = comp.first, h.isDigits {
+            return Int(h)
+        }
+
+        return nil
+    }
+
+    /// minutes from string looks like "HH:mm"
+    public static func minutes(from str: String) -> Int? {
+        let comp = str.components(separatedBy: ":")
+
+        if comp.count > 1, comp[1].isDigits {
+            return Int(comp[1])
+        }
+
+        return nil
     }
 }
