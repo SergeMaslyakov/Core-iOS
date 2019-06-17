28c4f72afb2ad5a1c720f159c81a4cf8fae013d3
Serge Maslyakov <rusoil.9@gmail.com>
2019-06-17 15:01:22 +0300

    - wip

---
+++ Core/Utils/AttributedStringUtils.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+public enum AttributedStringUtils {
+
+    public static func makeTextLink(text: String, textLinkRange: NSRange?, font: UIFont, textColor: UIColor, linkColor: UIColor) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: text, attributes: [
+                .font: font,
+                .foregroundColor: textColor,
+                .kern: -0.3
+            ])
+
+        let nsRange = textLinkRange ?? NSRange(location: 0, length: text.count)
+
+        /// I don't want how to set link color. Notify me if you know it
+        //let link = text[Range(nsRange, in: text) ?? text.startIndex..<text.endIndex]
+        //attributedString.addAttribute(.link, value: link, range: nsRange)
+        attributedString.addAttribute(.foregroundColor, value: linkColor, range: nsRange)
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: nsRange)
+        attributedString.addAttribute(.underlineColor, value: linkColor, range: nsRange)
+
+        return NSAttributedString(attributedString: attributedString)
+    }
+
+}
