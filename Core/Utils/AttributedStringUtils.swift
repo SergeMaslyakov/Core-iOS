f54d2b40d382d38be72d7f461b3cbe53d9ae27b0
Serge Maslyakov <serge.maslyakov@gmail.com>
2019-06-26 06:49:58 -0700

    - wip

--- Core/Utils/AttributedStringUtils.swift
+++ Core/Utils/AttributedStringUtils.swift
@@ -2,21 +2,22 @@ import UIKit
 
 public enum AttributedStringUtils {
 
-    public static func makeTextLink(text: String, textLinkRange: NSRange?, font: UIFont, textColor: UIColor, linkColor: UIColor) -> NSAttributedString {
+    public static func makeTextLink(text: String, textLinkRange: [NSRange], font: UIFont, textColor: UIColor, linkColor: UIColor) -> NSAttributedString {
         let attributedString = NSMutableAttributedString(string: text, attributes: [
                 .font: font,
-                .foregroundColor: textColor,
-                .kern: -0.3
+                .foregroundColor: textColor
             ])
 
-        let nsRange = textLinkRange ?? NSRange(location: 0, length: text.count)
+        let ranges = textLinkRange.isEmpty ? [NSRange(location: 0, length: text.count)] : textLinkRange
 
-        /// I don't want how to set the link color.
-        //let link = text[Range(nsRange, in: text) ?? text.startIndex..<text.endIndex]
-        //attributedString.addAttribute(.link, value: link, range: nsRange)
-        attributedString.addAttribute(.foregroundColor, value: linkColor, range: nsRange)
-        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: nsRange)
-        attributedString.addAttribute(.underlineColor, value: linkColor, range: nsRange)
+        ranges.forEach { range in
+            /// I don't want how to set the link color.
+            //let link = text[Range(nsRange, in: text) ?? text.startIndex..<text.endIndex]
+            //attributedString.addAttribute(.link, value: link, range: nsRange)
+            attributedString.addAttribute(.foregroundColor, value: linkColor, range: range)
+            attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: range)
+            attributedString.addAttribute(.underlineColor, value: linkColor, range: range)
+        }
 
         return NSAttributedString(attributedString: attributedString)
     }
---
+++ Core/Utils/UIDevice+Utils.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+public extension UIDevice {
+
+    enum Device: CaseIterable {
+        case iphone5
+        case iphone6
+        case iphone6Plus
+        case iphoneXR
+        case iphoneX
+        case iphoneXMax
+
+        var screenHeight: CGFloat {
+            switch self {
+            case .iphone5:
+                return 1136
+            case .iphoneX:
+                return 2436
+            case .iphoneXMax:
+                return 2688
+            case .iphoneXR:
+                return 1792
+            case .iphone6:
+                return 1134
+            case .iphone6Plus:
+                return 2208
+            }
+        }
+
+        init?(height: CGFloat) {
+            guard let device = Device.allCases.first(where: { $0.screenHeight == height }) else  { return nil }
+            self = device
+        }
+    }
+
+    static let device: Device = Device(height: UIScreen.main.nativeBounds.height) ?? .iphone6
+
+    var isIphone5: Bool {
+        return UIDevice.device == .iphone5
+    }
+
+}
