5d065c0c863e177d54d2f2d2d9a823bcc24a0c51
Serge Maslyakov <serge.maslyakov@gmail.com>
2019-09-18 14:34:40 +0300

    - network monitor

--- Core/Workers/NetworkMonitoring/NetworkQualityMonitoring.swift
+++ Core/Workers/NetworkMonitoring/NetworkQualityMonitoring.swift
@@ -4,17 +4,16 @@ import CoreTelephony
 import RxCocoa
 import RxSwift
 
-final public class NetworkQualityMonitoring {
+final public class NetworkQualityMonitoring: NSObject {
 
     private let disposeBag = DisposeBag()
-    private var notificationToken: NotificationToken?
 
     private let serialQueue: DispatchQueue
     private let telephonyInfo: CTTelephonyNetworkInfo
     private let reachabilityService: NetworkReachabilityService
 
-    private let radioTechnology = BehaviorRelay<RadioTechnologyType>(value: .unknown)
-    private let quality = BehaviorRelay<NetworkQualityType>(value: .unknown)
+    private let radioRelay: BehaviorRelay<RadioTechnologyType>
+    private let qualityRelay: BehaviorRelay<NetworkQualityType>
 
     private let logger: Logger
 
@@ -30,14 +29,27 @@ final public class NetworkQualityMonitoring {
             return nil
         }
 
-        self.networkQuality = quality.asObservable().share(replay: 1, scope: .forever)
+        if #available(iOS 12.0, *) {
+            let values = telephonyInfo.serviceCurrentRadioAccessTechnology?.values.map { RadioTechnologyType.make(from: $0) }
+            let value = values?.first(where: { $0 != .unknown }) ?? .unknown
+            self.radioRelay = BehaviorRelay<RadioTechnologyType>(value: value)
+        } else {
+            let value = RadioTechnologyType.make(from: telephonyInfo.currentRadioAccessTechnology ?? "")
+            self.radioRelay = BehaviorRelay<RadioTechnologyType>(value: value)
+        }
+
+        self.qualityRelay = BehaviorRelay<NetworkQualityType>(value: .unknown)
+        self.networkQuality = qualityRelay.asObservable().share(replay: 1, scope: .forever)
+
+        super.init()
 
         Observable
-            .combineLatest(radioTechnology, reachabilityService.reachabilityStatus)
+            .combineLatest(radioRelay, reachabilityService.reachabilityStatus)
             .skip(1)
             .map { NetworkQualityType.make(fromRadio: $0, andReachability: $1) }
             .asDriver(onErrorJustReturn: .unknown)
-            .drive(quality)
+            .do(onNext: { print($0) })
+            .drive(qualityRelay)
             .disposed(by: disposeBag)
     }
 
@@ -47,20 +59,20 @@ final public class NetworkQualityMonitoring {
 
     public func start() {
 
-        if #available(iOS 12.1, *) {
-            notificationToken = NotificationCenter.default.observe(name: .CTServiceRadioAccessTechnologyDidChange) { [weak self] _ in
-                self?.serialQueue.async {
-                    self?.radioAccessTechnologyDidChange_ios12()
+        if #available(iOS 13.0, *) {
+            telephonyInfo.delegate = self
+        } else if #available(iOS 12.0, *) {
+            telephonyInfo.serviceSubscriberCellularProvidersDidUpdateNotifier = { [unowned self] identifier in
+                self.serialQueue.async {
+                    let value = RadioTechnologyType.make(from: identifier)
+                    self.radioRelay.accept(value)
                 }
             }
-        } else if #available(iOS 12.0, *) {
-            /// https://openradar.appspot.com/46873673
-            /// App crashes while reading CTTelephonyNetworkInfo.serviceCurrentRadioAccessTechnology - iOS 12.0 only
-            logger.info("Skip subscribing for .CTServiceRadioAccessTechnologyDidChange")
         } else {
-            notificationToken = NotificationCenter.default.observe(name: .CTRadioAccessTechnologyDidChange) { [weak self] _ in
-                self?.serialQueue.async {
-                    self?.radioAccessTechnologyDidChange()
+            telephonyInfo.subscriberCellularProviderDidUpdateNotifier = { [unowned self] carrier in
+                self.serialQueue.async {
+                    let value = RadioTechnologyType.make(from: self.telephonyInfo.currentRadioAccessTechnology ?? "")
+                    self.radioRelay.accept(value)
                 }
             }
         }
@@ -69,25 +81,25 @@ final public class NetworkQualityMonitoring {
     }
 
     public func stop() {
-        notificationToken = nil
+        if #available(iOS 13.0, *) {
+            telephonyInfo.delegate = nil
+        } else if #available(iOS 12.0, *) {
+            telephonyInfo.serviceSubscriberCellularProvidersDidUpdateNotifier = nil
+        } else {
+            telephonyInfo.subscriberCellularProviderDidUpdateNotifier = nil
+        }
         reachabilityService.stop()
     }
+
 }
 
-private extension NetworkQualityMonitoring {
+extension NetworkQualityMonitoring: CTTelephonyNetworkInfoDelegate {
 
-    @available(iOS 12, *)
-    func radioAccessTechnologyDidChange_ios12() {
-        let values = telephonyInfo.serviceCurrentRadioAccessTechnology?.values.map { RadioTechnologyType.make(from: $0) }
-        let value = values?.first(where: { $0 != .unknown }) ?? .unknown
-
-        radioTechnology.accept(value)
-    }
-
-    func radioAccessTechnologyDidChange() {
-        let value = RadioTechnologyType.make(from: telephonyInfo.currentRadioAccessTechnology ?? "")
-
-        radioTechnology.accept(value)
+    public func dataServiceIdentifierDidChange(_ identifier: String) {
+        serialQueue.async {
+            let value = RadioTechnologyType.make(from: identifier)
+            self.radioRelay.accept(value)
+        }
     }
 
 }
