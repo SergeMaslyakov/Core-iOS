a5d71d67ee4aed8f4bbf14961c79211dc63c7b63
Serge Maslyakov <serge.maslyakov@gmail.com>
2019-11-21 18:25:18 +0300

    - removed shared schedulers form ConcurrentDispatchQueueScheduler

--- Core/Extensions/Rx/ConcurrentDispatchQueueScheduler+Factory.swift
+++ Core/Extensions/Rx/ConcurrentDispatchQueueScheduler+Factory.swift
@@ -5,10 +5,6 @@ public typealias ConcurrentRxScheduler = ConcurrentDispatchQueueScheduler
 
 public extension ConcurrentDispatchQueueScheduler {
 
-    static var sharedBackground: ConcurrentDispatchQueueScheduler = {
-        return makeConcurrentBackgroundScheduler("\(AppBundle.bundleIdentifier).background.shared.concurrent.scheduler")
-    }()
-
     static func makeConcurrentBackgroundScheduler(_ label: String = UUID().uuidString) -> ConcurrentDispatchQueueScheduler {
         return ConcurrentDispatchQueueScheduler(queue: DispatchQueue(label: label,
                                                                      qos: .background,
