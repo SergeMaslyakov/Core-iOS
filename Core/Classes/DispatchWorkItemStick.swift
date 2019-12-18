2b5c429b624e5d8f119efe1bd3da210f33b2f40c
Serge Maslyakov <serge.maslyakov@gmail.com>
2019-12-18 18:52:23 +0300

    - added DispatchWorkItemStick as wrapper for DispatchWorkItem

---
+++ Core/Classes/DispatchWorkItemStick.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public final class DispatchWorkItemStick {
+
+    public init() {}
+
+    private var _identifier: Int = 0
+
+    public var identifier: Int {
+        return _identifier
+    }
+
+    public var item: DispatchWorkItem? {
+        didSet {
+            _identifier += 1
+            oldValue?.cancel()
+        }
+    }
+
+    public func checkThatItemIsValid(_ identifier: Int) -> Bool {
+        guard _identifier == identifier else { return false }
+
+        return item != nil && item?.isCancelled == false
+    }
+
+    deinit {
+        item?.cancel()
+    }
+
+}
