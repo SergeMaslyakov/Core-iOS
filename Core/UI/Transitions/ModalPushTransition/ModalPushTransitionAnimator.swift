021e98d82507137659ccd8805137d8fa9cd01da9
Serge Maslyakov <serge.maslyakov@gmail.com>
2019-12-24 18:46:00 +0300

    - fixed modal-push transition

--- Core/UI/Transitions/ModalPushTransition/ModalPushTransitionAnimator.swift
+++ Core/UI/Transitions/ModalPushTransition/ModalPushTransitionAnimator.swift
@@ -14,18 +14,39 @@ public final class ModalPushTransitionAnimator: NSObject, UIViewControllerAnimat
     }
 
     public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        guard let fromVC = transitionContext.viewController(forKey: .from), let toVC = transitionContext.viewController(forKey: .to) else {
-            transitionContext.cancelInteractiveTransition()
+        let vcT = transitionContext.viewController(forKey: .to)
+        let vcF = transitionContext.viewController(forKey: .from)
+
+        guard let fromVC = vcF, let toVC = vcT else {
+            transitionContext.completeTransition(true)
             return
         }
 
-        let dX = presenting ? fromVC.view.frame.width : -fromVC.view.frame.width
+        let width = fromVC.view.bounds.width
 
-        toVC.view.frame = fromVC.view.frame.offsetBy(dx: dX, dy: 0)
-        let finalFrame = fromVC.view.frame.offsetBy(dx: -dX, dy: 0)
+        var initialToFrame = toVC.view.frame
+        var finalToFrame = toVC.view.frame
+
+        initialToFrame.origin.x = presenting ? width : -(width/3)
+        finalToFrame.origin.x = 0
+
+        toVC.view.frame = initialToFrame
+
+        var initialFromFrame = fromVC.view.frame
+        var finalFromFrame = fromVC.view.frame
+
+        initialFromFrame.origin.x = 0
+        finalFromFrame.origin.x = presenting ? -(width/3) : width
+
+        fromVC.view.frame = initialFromFrame
+
+        var completion: (() -> Void)?
 
         if presenting {
             transitionContext.containerView.addSubview(toVC.view)
+            completion = { [weak fromVC] in
+                fromVC?.view.frame = initialFromFrame
+            }
         }
         let duration = transitionContext.isAnimated ? transitionDuration : 0
 
@@ -35,9 +56,10 @@ public final class ModalPushTransitionAnimator: NSObject, UIViewControllerAnimat
                        initialSpringVelocity: 4, options: [.curveEaseOut],
                        animations: {
 
-            toVC.view.frame = fromVC.view.frame
-            fromVC.view.frame = finalFrame
+            toVC.view.frame = finalToFrame
+            fromVC.view.frame = finalFromFrame
         }, completion: { _ in
+            completion?()
             transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
         })
     }
