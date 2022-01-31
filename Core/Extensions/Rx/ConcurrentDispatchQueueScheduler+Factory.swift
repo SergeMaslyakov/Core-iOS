4c5e7fae5c374b73f4ab81c289624e9e01b0f942
Serge Maslyakov <smaslyakov@intermedia.net>
2022-01-31 15:50:22 +0400

    - wip

--- Core/Extensions/Rx/ConcurrentDispatchQueueScheduler+Factory.swift
+++ Core/Extensions/Rx/ConcurrentDispatchQueueScheduler+Factory.swift
@@ -3,6 +3,10 @@ import RxSwift
 public typealias ConcurrentRxScheduler = ConcurrentDispatchQueueScheduler
 
 public extension ConcurrentDispatchQueueScheduler {
+    static var sharedBackground: ConcurrentDispatchQueueScheduler = {
+        makeConcurrentBackgroundScheduler("\(AppBundle.bundleIdentifier).background.shared.concurrent.scheduler")
+    }()
+
     static func makeConcurrentBackgroundScheduler(_ label: String = UUID().uuidString) -> ConcurrentDispatchQueueScheduler {
         ConcurrentDispatchQueueScheduler(queue: DispatchQueue(label: label,
                                                               qos: .userInitiated,
